ed8b728fc9084358da4d848dd1f2099ff4dd3d4d
Gavo <gvmgust@gmail.com>
2023-03-29 16:56:12 -0400

    loading the map on canvas

--- game.js
+++ game.js
@@ -5,12 +5,31 @@ let elementSize;
 window.addEventListener("load", resizeCanvas);
 window.addEventListener("resize", resizeCanvas);
 
-function startGame() {}
+function startGame() {
+  game.font = elementSize + "px Verdana";
+  game.textAlign = "start";
+  game.textBaseline = "top";
+
+  const map = getMap(0);
+
+  map.forEach((rows, row) => {
+    rows.forEach((key, col) => {
+      const emoji = emojis[key];
+      const posX = col * elementSize - elementSize * 0.2;
+      const posY = row * elementSize + elementSize * 0.05;
+      game.fillText(emoji, posX, posY);
+    });
+  });
+}
 
 function resizeCanvas() {
-  let canvasSize = Math.min(window.innerHeight, window.innerWidth) * 0.85;
+  let canvasSize = Math.min(window.innerHeight, window.innerWidth) * 0.8;
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
   elementSize = canvasSize / 10;
   startGame();
+}
+
+function getMap(level) {
+  return maps[level].match(/[\-IXO]+/g).map((a) => a.split(""));
 }
