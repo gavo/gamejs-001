dfb8ed89d279f9fe5a1e1536e703b7c6932cbce3
Gavo <gvmgust@gmail.com>
2023-03-29 17:22:39 -0400

    Adding event listeners to buttons and keyboard

--- game.js
+++ game.js
@@ -1,9 +1,29 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnDown = document.querySelector("#down");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
 let elementSize;
 
 window.addEventListener("load", resizeCanvas);
 window.addEventListener("resize", resizeCanvas);
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp());
+btnDown.addEventListener("click", moveDown);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+
+function moveByKeys(event) {
+  if (event.code === "ArrowUp") moveUp();
+  else if (event.code === "ArrowDown") moveDown();
+  else if (event.code === "ArrowLeft") moveLeft();
+  else if (event.code === "ArrowRight") moveRight();
+}
+
+function getMap(level) {
+  return maps[level].match(/[\-IXO]+/g).map((a) => a.split(""));
+}
 
 function startGame() {
   game.font = elementSize + "px Verdana";
@@ -30,6 +50,15 @@ function resizeCanvas() {
   startGame();
 }
 
-function getMap(level) {
-  return maps[level].match(/[\-IXO]+/g).map((a) => a.split(""));
+function moveUp() {
+  console.log("Me muevo hacia arriba");
+}
+function moveDown() {
+  console.log("Me muevo hacia abajo");
+}
+function moveLeft() {
+  console.log("Me muevo a la izquierda");
+}
+function moveRight() {
+  console.log("Me muevo a la derecha");
 }
