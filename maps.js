baaccf108ab00ad34043d6993cc1bceecee89607
Gavo <gvmgust@gmail.com>
2023-03-30 18:41:30 -0400

    Added new maps, fixing css styles

--- maps.js
+++ maps.js
@@ -8,13 +8,29 @@ const emojis = {
   O: "🚪",
   X: "💣",
   I: "🎁",
-  PLAYER: "💀",
-  BOMB_COLLISION: "🔥",
+  PLAYER: "😺",
+  PLAYER_COLLISION: "🙀",
+  PLAYER_LEVELUP: "😸",
+  BOMB_COLLISION: "🎆", //
   GAME_OVER: "👎",
   WIN: "🏆",
+  HEART: "❤️",
 };
 
 const maps = [];
+
+maps.push(`
+    OXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    IXXXXXXXXX
+  `);
 maps.push(`
     IXXXXXXXXX
     -XXXXXXXXX
@@ -50,4 +66,88 @@ maps.push(`
     XX-XXXXXXX
     XX-----OXX
     XXXXXXXXXX
+  `);
+maps.push(`
+    OXXXXX---I
+    -XXXXX-XXX
+    -XXXXX-XXX
+    -XXXXX-XXX
+    -XX----XXX
+    -XX-XXXXXX
+    -XX-XXXXXX
+    -XX-XXXXXX
+    -XX-XXXXXX
+    ----XXXXXX
+  `);
+maps.push(`
+    XXXXXXXXXO
+    XXXXXXXXX-
+    X---------
+    X-XXXXXXXX
+    X-------XX
+    XXXXXXX-XX
+    XXXXXXX-XX
+    X-------XX
+    X-XXXXXXXX
+    X---IXXXXX
+`);
+maps.push(`
+    IXXX------
+    -XXX-XXXX-
+    -XXX-XXXX-
+    --XX-XXXX-
+    X-XX-XXXX-
+    X----XXXX-
+    XXXXXXXXX-
+    XXXX---XX-
+    XXXX-X-XX-
+    XXXXOX----
+`);
+maps.push(`
+    OX---X---I
+    -X-X-X-XXX
+    -X-X-X-XXX
+    -X-X-X-XXX
+    -X-X-X-XXX
+    -X-X-X-XXX
+    -X-X-X-XXX
+    -X-X-X-XXX
+    -X-X-X-XXX
+    ---X---XXX
+  `);
+maps.push(`
+  ---------O
+  -XXXXXXXXX
+  ----------
+  XXXXXXXXX-
+  ----------
+  -XXXXXXXXX
+  ----------
+  XXXXXXXXX-
+  ----------
+  IXXXXXXXXX
+`);
+maps.push(`
+    ---X---XXI
+    -X-X-X-X--
+    -X-X-X-X-X
+    -X-X-X-X-X
+    -X-X-X-X-X
+    -X-X-X-X-X
+    -X-X-X-X-X
+    -X-X-X-X-X
+    -X-X-X-X-X
+    OX---X---X
+`);
+maps.push(`
+    ---------O
+    -XXXXXXXXX
+    -X---X---X
+    -X-X---X--
+    -X-XXXXXX-
+    -X-----X--
+    -XXXXX-X-X
+    -X---X-X--
+    -X-X-X-XX-
+    ---X---XXI
   `);
